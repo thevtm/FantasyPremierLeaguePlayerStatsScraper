425b6c80ff01e228beafb2cf69ce543682b47607
TheVTM <tallibba@gmail.com>
2015-04-01 17:12:08 -0300

    Scraper 0.1

--- scraper.py
+++ scraper.py
@@ -1,23 +1,88 @@
-# This is a template for a Python scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+# <codecell>
 
-# You don't have to do things with the ScraperWiki and lxml libraries. You can use whatever libraries are installed
-# on Morph for Python (https://github.com/openaustralia/morph-docker-python/blob/master/pip_requirements.txt) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+## The MIT License (MIT)
+##
+## Copyright (c) 2015 Vinícius Tabille Manjabosco
+##
+## Permission is hereby granted, free of charge, to any person obtaining a copy
+## of this software and associated documentation files (the "Software"), to deal
+## in the Software without restriction, including without limitation the rights
+## to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+## copies of the Software, and to permit persons to whom the Software is
+## furnished to do so, subject to the following conditions:
+##
+## The above copyright notice and this permission notice shall be included in all
+## copies or substantial portions of the Software.
+##
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+## SOFTWARE.
+
+# <codecell>
+
+import itertools as it
+import json
+import scraperwiki
+import pandas as pd
+
+# <codecell>
+
+## Consts
+PLAYER_DATA_URL = "http://fantasy.premierleague.com/web/api/elements/"
+
+# <codecell>
+
+def ExtractPlayerDF(Data):
+    colNames = ['Date', 'Round', 'Opponent', 'MP', 'GS', 'A', 'CS', 'GC', 'OG', 'PS',
+                'PM', 'YC', 'RC', 'S', 'B', 'ESP', 'BPS', 'NT', 'Value', 'Points']
+    fixtures = Data['fixture_history']['all']
+    playerDF = pd.DataFrame(fixtures, columns = colNames)
+
+    playerDF['ID'] = Data['id']
+    playerDF['Code'] = Data['code']
+    playerDF['WebName'] = Data['web_name']
+    playerDF['FirstName'] = Data['first_name']
+    playerDF['SecondName'] = Data['second_name']
+    playerDF['Position'] = Data['type_name']
+    playerDF['Team'] = Data['team_name']
+
+    colOrder = ['ID', 'Code', 'Round', 'WebName', 'FirstName', 'SecondName', 'Position', 'Team',
+                'Date', 'Opponent', 'MP', 'GS', 'A', 'CS', 'GC', 'OG', 'PS', 'PM', 'YC',
+                'RC', 'S', 'B', 'ESP', 'BPS', 'NT', 'Value', 'Points']
+
+    return playerDF[colOrder]
+
+# <codecell>
+
+## Download data
+playersDataRaw = []
+
+for i in it.count(1):
+    url = PLAYER_DATA_URL + str(i)
+    try:
+        playerDataJson = scraperwiki.scrape(url)
+        playersDataRaw.append(json.loads(playerDataJson))
+    except:
+        print 'Last Player Index evaluated: ' + str(i)
+        break
+
+# <codecell>
+
+## Mine Players Data
+## and concat all into one DataFrame
+PlayersData = pd.concat(map(ExtractPlayerDF, playersDataRaw), ignore_index = True)
+
+# <codecell>
+
+## Save DataFrame to SQLite
+scraperwiki.sqlite.save(unique_keys = [],
+                        data = PlayersData.to_dict(outtype = 'records'),
+                        table_name = 'data')
+
